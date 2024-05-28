f5d648410601952d7fbd5ea8a1bd6cae9c83432e
BillArmsty <armsty9@gmail.com>
2024-05-28 10:43:49 +0300

    anchor implementation

--- programs/rustysolana/src/lib.rs
+++ programs/rustysolana/src/lib.rs
@@ -1,15 +1,44 @@
 use anchor_lang::prelude::*;
+use solana_program::entrypoint::ProgramResult;
 
-declare_id!("EzN6P2ZuFz8arhAHs59u5vSz2A5X2LFH6Cmb6iVzxX9g");
+declare_id!("DvNkYwKFYEpLvvTgZ9yCEoBwG37d54LxCbJ6X1fBzMyp");
 
 #[program]
-pub mod rustysolana {
+mod mysolanaapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
         Ok(())
     }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
--- tests/rustysolana.js
+++ tests/rustysolana.js
@@ -1,13 +1,53 @@
-const anchor = require("@coral-xyz/anchor");
+import * as anchor from "@project-serum/anchor";
+import { web3, setProvider } from "@project-serum/anchor";
+import { ok } from "assert";
+const { SystemProgram, Keypair } = web3;
+
+const { AnchorProvider } = anchor;
 
 describe("rustysolana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = AnchorProvider.env();
+  setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Rustysolana;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  const program = anchor.workspace.rustysolana;
+
+
+  let _baseAccount;
+  it("Creates a counter)", async () => {
+    //Call the create function via RPC
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    //Get the account data and check value of the counter
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("Count 0:", account.count.toString());
+    ok(account.count.toString() === "0");
+    _baseAccount = baseAccount;
+  });
+
+  it("Increments the counter", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("Count 1:", account.count.toString());
+    ok(account.count.toString() === "1");
   });
 });
